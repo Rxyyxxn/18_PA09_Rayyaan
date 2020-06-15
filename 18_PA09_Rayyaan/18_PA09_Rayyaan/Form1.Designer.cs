609f967e3cf5e906f38f3d77c4e8095d439d9db3
Rxyyxxn <rayyaanputra@gmail.com>
2020-06-15 13:59:54 +0800

    Currency convertor project created by rayyaan
    Currency convertor project created by rayyaan

---
+++ 18_PA09_Rayyaan/18_PA09_Rayyaan/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace _18_PA09_Rayyaan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Amount = new System.Windows.Forms.TextBox();
+            this.txt_converted = new System.Windows.Forms.TextBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.rb_US = new System.Windows.Forms.RadioButton();
+            this.rb_Yen = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select currency";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "converted amount";
+            // 
+            // txt_Amount
+            // 
+            this.txt_Amount.Location = new System.Drawing.Point(155, 98);
+            this.txt_Amount.Name = "txt_Amount";
+            this.txt_Amount.Size = new System.Drawing.Size(154, 20);
+            this.txt_Amount.TabIndex = 3;
+            // 
+            // txt_converted
+            // 
+            this.txt_converted.Location = new System.Drawing.Point(155, 247);
+            this.txt_converted.Name = "txt_converted";
+            this.txt_converted.Size = new System.Drawing.Size(154, 20);
+            this.txt_converted.TabIndex = 4;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(371, 172);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(75, 23);
+            this.btn_convert.TabIndex = 5;
+            this.btn_convert.Text = "convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(371, 230);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 6;
+            this.btn_clear.Text = "clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // rb_US
+            // 
+            this.rb_US.AutoSize = true;
+            this.rb_US.Location = new System.Drawing.Point(155, 170);
+            this.rb_US.Name = "rb_US";
+            this.rb_US.Size = new System.Drawing.Size(75, 17);
+            this.rb_US.TabIndex = 7;
+            this.rb_US.TabStop = true;
+            this.rb_US.Text = "US Dollars";
+            this.rb_US.UseVisualStyleBackColor = true;
+            this.rb_US.CheckedChanged += new System.EventHandler(this.rb_US_CheckedChanged);
+            // 
+            // rb_Yen
+            // 
+            this.rb_Yen.AutoSize = true;
+            this.rb_Yen.Location = new System.Drawing.Point(155, 193);
+            this.rb_Yen.Name = "rb_Yen";
+            this.rb_Yen.Size = new System.Drawing.Size(93, 17);
+            this.rb_Yen.TabIndex = 8;
+            this.rb_Yen.TabStop = true;
+            this.rb_Yen.Text = "Japanese Yen";
+            this.rb_Yen.UseVisualStyleBackColor = true;
+            this.rb_Yen.CheckedChanged += new System.EventHandler(this.rb_Yen_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 389);
+            this.Controls.Add(this.rb_Yen);
+            this.Controls.Add(this.rb_US);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.txt_converted);
+            this.Controls.Add(this.txt_Amount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Amount;
+        private System.Windows.Forms.TextBox txt_converted;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.RadioButton rb_US;
+        private System.Windows.Forms.RadioButton rb_Yen;
+    }
+}
+
---
+++ 18_PA09_Rayyaan/18_PA09_Rayyaan/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _18_PA09_Rayyaan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rb_US_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rb_Yen_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            const decimal us = 0.74M, yen = 81.97M;
+
+            try
+            {
+
+                decimal Amount = Convert.ToDecimal(txt_Amount.Text);
+                if (rb_US.Checked)
+                {
+                    decimal answer = Amount * us;
+                    txt_converted.Text = answer.ToString();
+                }
+                if (rb_Yen.Checked)
+                {
+                    decimal answer = Amount * yen;
+                    txt_converted.Text = answer.ToString();
+                }
+                
+                else
+                {
+                    txt_Amount.Text = "Choose one of the currencies";
+                }
+
+            }
+            catch (FormatException)
+            {
+                txt_Amount.Text = "Please enter a valid amount";
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_converted.Clear();
+            txt_Amount.Clear();
+            rb_US.Checked = false;
+            rb_Yen.Checked = false;
+        }
+    }
+}
